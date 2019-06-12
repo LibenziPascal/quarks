73d44cec837b71e9e9a0489d07a2c97bf3c1d9be
Libs <pascal.libenzi@gmail.com>
2019-06-12 21:01:59 +0200

    simple test quarks to build image

---
+++ libs-quarks/src/test/java/com/libs/quarks/NativeQuarkusLibsIT.java
@@ -0,0 +1,9 @@
+package com.libs.quarks;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeQuarkusLibsIT extends QuarkusLibsTest {
+
+    // Execute the same tests but in native mode.
+}
